bc5f7e32fb679bc9e229317942a81fabedd83771
samir-ben <samir-69@hotmail.fr>
2019-02-17 14:44:36 +0100

    feat: stock cards
    - created stock cards template

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,7 +1,19 @@
 <template>
-  <v-container>
-    <h1>Stock/Stock</h1>
-  </v-container>
+   <v-layout align-center class="mb-5">
+      <v-flex sm6 md4>
+        <v-card  width="300" height="130">
+          <v-card-title class="card-title">
+            <h3>Titre <small> (Price: PRICE )</small></h3>
+          </v-card-title>
+          <v-card-text>
+            <v-layout row class="pb-5"> 
+                <v-text-field placeholder="Quantity" solo type="number"></v-text-field>
+                <v-btn color="info" class="ml-2">Buy</v-btn>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -10,6 +22,10 @@
   }
 </script>
 
-<style>
-
+<style scope>
+.card-title{
+  height: 30px;
+  background-color: rgb(168, 216, 242);
+  padding-bottom: 30px;
+}
 </style>
