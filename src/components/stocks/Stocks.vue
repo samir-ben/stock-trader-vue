8e0cc42355eafbd432cb16e9208ba8f245a0e6ae
samir-ben <samir-69@hotmail.fr>
2019-02-17 14:44:36 +0100

    feat: stock cards
    - created stock cards template

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,12 +1,28 @@
 <template>
   <v-container>
-    <h1>Stock/stocks</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id"></Stock>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
+import Stock from './Stock'
   export default {
- 
+    components: {
+      Stock
+    },
+    data() {
+      return {
+        stocks: [
+          {id: '1', content: "BMW", price: 200},
+          {id: '2', content: "Google", price: 400},
+          {id: '3', content: "IBM", price: 250},
+          {id: '4', content: "Facebook", price: 300},
+          {id: '5', content: "Twitter", price: 120}
+        ]
+      }
+    },
   }
 </script>
 
