a6049d4728a799a631b66a4f4bf120ecaf8be954
samir-ben <samir-69@hotmail.fr>
2019-02-17 12:17:45 +0100

    feat: routing
    - add two routes in router file
    - add view-router tag in app vue file

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
+import Portfolio from './components/portfolio/Portfolio.vue'
+import Stocks from './components/stocks/Stocks.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio
+    },
+    {
+      path: '/stocks',
+      name: 'stocks',
+      component: Stocks
     },
   ]
 })
