70c7b853b2ebf750ce63620f5129aa8b63ebe090
samir-ben <samir-69@hotmail.fr>
2019-02-17 17:42:08 +0100

    feat: portfolio store

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import stocks from './modules/stocks';
+import porfolio from './modules/porfolio';
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     stocks,
+    porfolio
   },
 });
